e7c478c3dc3c9d31be02e9bef3416cab8335b66f
Nbiba Bedis <bedisnbiba@gmail.com>
2021-07-09 12:41:35 +0100

    style wunzip

--- initial-utilities/wunzip/wunzip.rs
+++ initial-utilities/wunzip/wunzip.rs
@@ -1,37 +1,38 @@
 #![feature(maybe_uninit_array_assume_init)]
+#![allow(clippy::many_single_char_names)]
 use std::fs::File;
 use std::io::{BufReader, Read, Write};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 const USAGE: &str = "wunzip: file1 [file2 ...]";
 
-struct IterChunks<I, T, const C: usize>
-where
-    I: Iterator<Item = T>,
-{
-    iter: I,
-}
+fn iter_chunks<T, I: Iterator<Item = T>, const C: usize>(iter: I) -> impl Iterator<Item = [T; C]> {
+    struct IterChunks<I, T, const C: usize>
+    where
+        I: Iterator<Item = T>,
+    {
+        iter: I,
+    }
 
-use std::mem::MaybeUninit;
+    use std::mem::MaybeUninit;
 
-impl<T, I: Iterator<Item = T>, const C: usize> Iterator for IterChunks<I, T, C> {
-    type Item = [T; C];
-    fn next(&mut self) -> Option<Self::Item> {
-        unsafe {
-            let mut a: [MaybeUninit<T>; C] = MaybeUninit::uninit().assume_init();
-            for i in 0..C {
-                let next = match self.iter.next() {
-                    Some(n) => n,
-                    None => return None,
-                };
-                let next = MaybeUninit::new(next);
-                a.as_mut_ptr().offset(i as isize).write(next);
+    impl<T, I: Iterator<Item = T>, const C: usize> Iterator for IterChunks<I, T, C> {
+        type Item = [T; C];
+        fn next(&mut self) -> Option<Self::Item> {
+            unsafe {
+                let mut a: [MaybeUninit<T>; C] = MaybeUninit::uninit().assume_init();
+                for i in 0..C {
+                    let next = match self.iter.next() {
+                        Some(n) => n,
+                        None => return None,
+                    };
+                    let next = MaybeUninit::new(next);
+                    a.as_mut_ptr().add(i).write(next);
+                }
+                Some(MaybeUninit::array_assume_init(a))
             }
-            Some(MaybeUninit::array_assume_init(a))
         }
     }
-}
-fn iter_chunks<T, I: Iterator<Item = T>, const C: usize>(iter: I) -> IterChunks<I, T, C> {
     IterChunks { iter }
 }
 
@@ -52,19 +53,13 @@ fn main() {
     }
 }
 
-#[derive(Debug)]
-struct S {
-    last_byte: Option<u8>,
-    c: u32,
-}
 fn unzip(file: BufReader<impl Read>) {
     let chunks = iter_chunks::<_, _, 5>(file.bytes().map(std::result::Result::unwrap));
     for chunk in chunks {
         let [a, b, c, d, ascii] = chunk;
         let n: u32 = u32::from_ne_bytes([a, b, c, d]);
         std::io::stdout()
-            .write(&[ascii].repeat(n as usize))
+            .write_all(&[ascii].repeat(n as usize))
             .unwrap();
     }
 }
-
